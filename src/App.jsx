d21ac87ed8b05b19b523cbca505ee2f57cfcf590
KatzuneStarky <aramirez_17@alu.uabcs.mx>
2022-03-15 12:46:24 -0700

    Reinicio de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import './App.css';
 import Footer from './Components/Footer/Footer';
 import Menu from './Components/Menu/Menu';
 import HomePage from './Pages/HomePage/HomePage';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Contact from './Pages/Contact/Contact';
+import { useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App"> 
-      <Menu />
-      <HomePage />
-      <Footer />
+    <div className="App">     
+      <Router>
+        <Menu />
+        <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route path='/about' element={<Contact />} />
+            <Route path='/portfolio' element={<Contact />} />
+            <Route path='/team' element={<Contact />} />
+            <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>    
     </div>
   );
 }
